57e71ff7106e44836b1a5401ae7cfb1ddfbae910
Gabriel Rozendo <gabrielrozendodacosta@gmail.com>
2025-04-03 12:37:54 -0300

    feat: Adiciona camada Model e a classe PessoaModel

---
+++ src/main/java/br/com/projeto/api/model/PessoaModel.java
@@ -0,0 +1,25 @@
+package br.com.projeto.api.model;
+
+public class PessoaModel {
+
+    // Atributo
+    private String nome;
+    private int idade;
+
+    // Get e Set
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
