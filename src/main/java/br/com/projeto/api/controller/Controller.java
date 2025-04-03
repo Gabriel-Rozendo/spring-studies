114a8c600b64e7529b8966120188b1357d62235f
Gabriel Rozendo <gabrielrozendodacosta@gmail.com>
2025-04-03 12:31:04 -0300

    feat: Adiciona duas rotas Get, uma com o @PathVariable e outra sem

--- src/main/java/br/com/projeto/api/controller/Controller.java
+++ src/main/java/br/com/projeto/api/controller/Controller.java
@@ -1,6 +1,7 @@
 package br.com.projeto.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,15 @@ public class Controller {
     @GetMapping("/ola")
     public String mensagem() {
         return "Olá, Mundo!";
+    }
+
+    @GetMapping("/boasvindas/{nome}")
+    public String boasVindas(@PathVariable String nome) {
+        return "Seja bem-vindo(a) " + nome;
+    }
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Seja bem-vindo(a)";
     }
 }
